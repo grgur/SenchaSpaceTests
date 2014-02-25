c650fbb7b0110495b01a4e6818b1e5a5e2a1e5d2
Grgur Grisogono <grguru@gmail.com>
2014-02-25 10:38:08 +0100

    Added a note to show when Sencha Space is detected

--- spacer/app/view/Main.js
+++ spacer/app/view/Main.js
@@ -5,5 +5,13 @@ Ext.define('Spacer.view.Main', {
     config: {
         title: 'Spacer',
         html: 'This is <b>Spacer</b>, our first Sencha Space app.'
+    },
+
+    applyHtml: function (html) {
+        if (Ext.isSpace) {
+            html += '<br />Hey, I noticed you are using Sencha Space. Werd!';
+        }
+
+        return html;
     }
 });
