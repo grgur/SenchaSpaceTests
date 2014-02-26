7e83d4f6cd17fe081260c07abbe87964e1d1767f
Grgur Grisogono <grguru@gmail.com>
2014-02-26 15:15:53 +0100

    Sending to spacer

--- warp/app/controller/Communication.js
+++ warp/app/controller/Communication.js
@@ -13,23 +13,14 @@ Ext.define('Warp.controller.Communication', {
 
         main.log('Space is ready with the built in Controller.prototype.onSpaceReady method');
         Ext.Msg.prompt('Message', 'Send something to Spacer', function (button, text) {
-            main.log('pressed ', button);
-            main.log('text ', text);
-
-            console.log('[button pressed]', button);
+            main.log('Sending to spacer: ', text);
 
             if (button === 'ok') {
                 me.sendSpaceMsg({
                     app     : 'spacer',
                     action  : 'texting',
                     msg     : text,
-                    show    : true,
-                    success : function (m) {
-                        main.log('Msg sent successfully ' + m);
-                    },
-                    failure : function (m) {
-                        main.log('Msg failed ' + m);
-                    }
+                    show    : true
                 });
             }
         });
